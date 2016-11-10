0f52797ea3bb8692212ca13c1b3e6227a057fc23
Eric McAllister <ericmcallister1@gmail.com>
2016-11-10 21:41:54 +0900

    Fixing arrow to stay anchored at bottom of splash page

--- templates/splash.php
+++ templates/splash.php
@@ -1,9 +1,17 @@
 <div class="intro splash">
   <div class="col-sm-4 col-sm-offset-4 animated fadeInUpBig">
-    <p>We <span>love</span> to travel.</p>
+    <p class="five-percent">We <span>love</span> to travel.</p>
     <p>...and do bathrooms</p>
+
+  </div>
+</div>
+<div class="home-arrow">
+  <div class="col-sm-4 col-sm-offset-4 animated fadeInUpBig">
     <a href="#">
-      <span class="glyphicon glyphicon-circle-arrow-down"></span>
+      <p>
+        (see what we're up to)
+      </p>
+      <span class="glyphicon glyphicon-circle-arrow-down ten-percent"></span>
     </a>
   </div>
 </div>
