0ec69f0abdab732624a77ddf7811aefd953f49e8
Eric McAllister <ericmcallister1@gmail.com>
2016-11-10 21:06:40 +0900

    Making sure splash css is correct iwth nav

--- templates/splash.php
+++ templates/splash.php
@@ -1,5 +1,4 @@
 <div class="intro splash">
-<?php get_template_part('templates/page', 'header'); ?>
   <div class="col-sm-4 col-sm-offset-4 animated fadeInUpBig">
     <p>We <span>love</span> to travel.</p>
     <p>...and do bathrooms</p>
