26a3e8d44c62577a98a90fc0561da7e7570d1bb6
Eric McAllister <ericmcallister1@gmail.com>
2016-11-10 20:55:34 +0900

    Creating new file, home.php and linking properly

---
+++ templates/splash.php
@@ -0,0 +1,10 @@
+<div class="intro splash">
+<?php get_template_part('templates/page', 'header'); ?>
+  <div class="col-sm-4 col-sm-offset-4 animated fadeInUpBig">
+    <p>We <span>love</span> to travel.</p>
+    <p>...and do bathrooms</p>
+    <a href="#">
+      <span class="glyphicon glyphicon-circle-arrow-down"></span>
+    </a>
+  </div>
+</div>
