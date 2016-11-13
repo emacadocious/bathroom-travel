b7e86c8a6c9539f18fbd7e8813d9d6974f868074
Eric McAllister <ericmcallister1@gmail.com>
2016-11-13 13:15:00 +0900

    Making sure intro text is centered on page

--- templates/splash.php
+++ templates/splash.php
@@ -1,14 +1,20 @@
-<div class="intro splash">
-  <div class="col-sm-4 col-sm-offset-4 animated fadeInUpBig">
-    <p class="five-percent">We <span>love</span> to travel.</p>
-    <p>...and do bathrooms</p>
+<div class="container">
+  <div class="span12">
+    <div class="intro">
+      <div class="splash">
+        <div class="animated fadeInUpBig">
+          <p >We <span>love</span> to travel.</p>
+          <p>...and do bathrooms</p>
 
-  </div>
-</div>
-<div class="home-arrow">
-  <div class="col-sm-4 col-sm-offset-4 animated fadeInUpBig">
-    <a href="#home-article">
-      <span class="glyphicon glyphicon-circle-arrow-down ten-percent"></span>
-    </a>
+        </div>
+      </div>
+      <div class="home-arrow">
+        <div class="animated fadeInUpBig">
+          <a href="#home-article">
+            <span class="glyphicon glyphicon-circle-arrow-down"></span>
+          </a>
+        </div>
+      </div>
+    </div>
   </div>
 </div>
