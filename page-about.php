4bdf60c9d973576e37cdb3453e558c6620937d86
Eric McAllister <ericmcallister1@gmail.com>
2016-11-16 13:53:18 +0900

    More progress on about apge

--- page-about.php
+++ page-about.php
@@ -8,25 +8,46 @@
  */
  ?>
 
+
 <div class="container">
   <div class="about-us">
     <div class="row">
       <h1>who are we?</h1>
     </div>
     <div class="row story">
-      <div class="col-sm-3 eric">
-        <img src="<?php bloginfo('template_url'); ?>/assets/images/eric-scooter.png" class="img-responsive" />
-        <h2>eric (emac)</h2>
+      <div class="col-md-3 eric">
+        <div class="hovereffect">
+          <img src="<?php bloginfo('template_url'); ?>/assets/images/eric-scooter.png" class="img-responsive" />
+          <div class="overlay">
+             <h2><span class="white">likes</span></h2>
+             <h2>being an ass</h2>
+             <h2>craft beer</h2>
+             <h2>headphones</h2>
+          </div>
+        </div>
+        <div class="row">
+          <h2>eric (emac)</h2>
+        </div>
       </div>
-      <div class="col-sm-6 bio">
+      <div class="col-md-6 bio">
         <p>we moved to <span class="location">seoul, south korea</span> in july of 2016. we haven't looked back since</p>
         <p class="middle">together, we've been to many places around the world.</p>
         <p>we both have separate hobbies, but we're very thankful for everything.</p>
         <p>sometimes, we like to talk about things other than travel. (like beer)</p>
       </div>
-      <div class="col-sm-3 jackie pull-right">
-        <img src="<?php bloginfo('template_url'); ?>/assets/images/eric-monkey.png" class="img-responsive" />
-        <h2>jackie (jack)</h2>
+      <div class="col-md-3 jackie">
+        <div class="hovereffect">
+          <img src="<?php bloginfo('template_url'); ?>/assets/images/jackie-monkey.png" class="img-responsive" />
+          <div class="overlay">
+             <h2><span class="white">likes</span></h2>
+             <h2>horsies</h2>
+             <h2>traveling</h2>
+             <h2>cocktails</h2>
+          </div>
+        </div>
+        <div class="row">
+          <h2>jackie (jack)</h2>
+        </div>
       </div>
     </div>
     <div class="row">
