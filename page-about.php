dd6ecf7bbaaec2f5297c35f6fdc4d424202e9348
Eric McAllister <ericmcallister1@gmail.com>
2016-11-17 17:43:43 +0900

    Adding markup for about, adding new font

--- page-about.php
+++ page-about.php
@@ -9,13 +9,13 @@
  ?>
 
 
-<div class="container">
+<div class="container-fluid">
   <div class="about-us">
     <div class="row">
       <h1>who are we?</h1>
     </div>
     <div class="row story">
-      <div class="col-md-3 eric">
+      <div class="col-lg-3 col-md-4 eric">
         <div class="hovereffect">
           <img src="<?php bloginfo('template_url'); ?>/assets/images/eric-scooter.png" class="img-responsive" />
           <div class="overlay">
@@ -25,33 +25,37 @@
              <h2>headphones</h2>
           </div>
         </div>
-        <div class="row">
+        <div class="clearfix visible-md-up"></div>
+        <div class="description">
           <h2>eric (emac)</h2>
+          <h4>I'm a web developer who loves to drink beer.</h4>
         </div>
       </div>
-      <div class="col-md-6 bio">
+      <div class="clearfix visible-sm"></div>
+      <div class="col-lg-6 col-md-4 bio">
         <p>we moved to <span class="location">seoul, south korea</span> in july of 2016. we haven't looked back since</p>
         <p class="middle">together, we've been to many places around the world.</p>
         <p>we both have separate hobbies, but we're very thankful for everything.</p>
         <p>sometimes, we like to talk about things other than travel. (like beer)</p>
       </div>
-      <div class="col-md-3 jackie">
+      <div class="col-lg-3 col-md-4 jackie">
         <div class="hovereffect">
           <img src="<?php bloginfo('template_url'); ?>/assets/images/jackie-monkey.png" class="img-responsive" />
           <div class="overlay">
-             <h2><span class="white">likes</span></h2>
-             <h2>horsies</h2>
-             <h2>traveling</h2>
-             <h2>cocktails</h2>
+            <h2><span class="white">likes</span></h2>
+            <h2>horsies</h2>
+            <h2>traveling</h2>
+            <h2>cocktails</h2>
           </div>
         </div>
-        <div class="row">
+        <div class="description">
           <h2>jackie (jack)</h2>
-        </div>
+          <h4>I'm a traveler who teaches first grade.</h4>
       </div>
     </div>
-    <div class="row">
-      <h1>we like to write about 3 things we are thankful for, at the end of each post.</h1>
-    </div>
+  </div>
+  </div>
+  <div class="row">
+    <h1>we like to write about 3 things we are thankful for, at the end of each post.</h1>
   </div>
 </div>
