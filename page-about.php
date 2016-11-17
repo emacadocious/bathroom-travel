42512c63793cb09f9e15179a6e31ee927b0816d4
Eric McAllister <ericmcallister1@gmail.com>
2016-11-17 18:36:16 +0900

    Last tweaks

--- page-about.php
+++ page-about.php
@@ -34,26 +34,30 @@
         </div>
       </div>
       <div class="clearfix visible-sm"></div>
-      <div class="col-lg-6 col-md-4 bio">
-        <p>we moved to <span class="location">seoul, south korea</span> in july of 2016. we haven't looked back since</p>
-        <p class="middle">together, we've been to many places around the world.</p>
-        <p>we both have separate hobbies, but we're very thankful for everything.</p>
-        <p>sometimes, we like to talk about things other than travel. (like beer)</p>
+      <div class="col-lg-6 col-md-4">
+        <div class="bio">
+          <p>we moved to <span class="location">seoul, south korea</span> in july of 2016. we haven't looked back since</p>
+          <p class="middle">together, we've been to many places around the world.</p>
+          <p>we both have separate hobbies, but we're very thankful for everything.</p>
+          <p>sometimes, we like to talk about things other than travel. (like beer)</p>
+        </div>
       </div>
       <div class="col-lg-3 col-md-4 jackie">
-        <div class="hovereffect">
-          <img src="<?php bloginfo('template_url'); ?>/assets/images/jackie-monkey.png" class="img-responsive" />
-          <div class="overlay">
-            <h2><span class="white">likes</span></h2>
-            <h2>horsies</h2>
-            <h2>traveling</h2>
-            <h2>cocktails</h2>
+        <div class="row">
+          <div class="hovereffect col-xs-12 col-sm-6 col-md-12">
+            <img src="<?php bloginfo('template_url'); ?>/assets/images/jackie-monkey.png" class="img-responsive" />
+            <div class="overlay">
+              <h2><span class="white">likes</span></h2>
+              <h2>horsies</h2>
+              <h2>traveling</h2>
+              <h2>cocktails</h2>
+            </div>
           </div>
+          <div class="clearfix hidden-sm"></div>
+          <div class="description col-xs-12 col-sm-6 col-md-12">
+            <h2>jackie (jack)</h2>
+            <h4>I'm a traveler who teaches first grade.</h4>
         </div>
-        <div class="clearfix hidden-sm"></div>
-        <div class="description">
-          <h2>jackie (jack)</h2>
-          <h4>I'm a traveler who teaches first grade.</h4>
       </div>
     </div>
   </div>
