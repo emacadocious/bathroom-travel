3166d08c94dd9d03b6103ebccd1ce6230fb10b67
Eric McAllister <ericmcallister1@gmail.com>
2016-11-19 16:53:39 +0900

    Removing about us.php

--- templates/about-us.php
+++
@@ -1,8 +0,0 @@
-<div id="row">
-  asdkjfhaksjhfkjhs
-  <div class="col-md-8 col-md-offset-2 box animated fadeInUpBig">
-    <div class="welcome-text">
-      <?php echo _e("we travel <br />and do bathrooms"); ?>
-    </div>
-  </div>
-</div>
